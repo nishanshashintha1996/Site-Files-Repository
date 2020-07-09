15ee331ff504c13507e083af0ec1c6273e0f8ae9
Nishan Shashintha <45489545+nishanshashintha1996@users.noreply.github.com>
2020-07-09 08:01:24 +0530

    Expression

--- main.c
+++ main.c
@@ -1,61 +1,101 @@
-
-
-//R.M.N.S.Samarakoon
-//CT/2015/054
-
-
 #include <stdio.h>
-#include "head.h"
+#include "header.h"
 #include <string.h>
 
 void main()
 {
-//----------Initialize variables-------------//
-    int x,y,yescount=0;Stack S;
-    char text[1000],modified1[1000],c,temp;
-//----------getting text field-------------//
+
+    int x,y,z,type_1=0,type_2=0,type_3=0;Stack S;
+    char text[1000],modified[1000],ts_arr[1000],c;
+    char type1_1='{';  char type1_2='}';  char type2_1='[';  char type2_2=']';  char type3_1='(';  char type3_2=')';
+    //----------getting text field-------------//
     CreateStack(&S);
-    printf("\n\n\n\n\t     A palindrome is a word, number, phrase, or other sequence of \n\t     characters which reads the same backward as forward.\n\t     This program is familiar with characters.-------\n\n\t      Enter String did you want :\t");
+    printf("\n\n\n\n\t\tEnter expression did you want to check :");
     gets(text);
     x = strlen(text);
+
     //printf("\nlength of text :%d\n\n",x);
     int count=0;
 
-    for(int i=0; i<=x; i++)
+    for(int i=0; i<x; i++)
     {
-        if (text[i] <= 'Z' && text[i] >= 'A' || text[i] <= 'z' && text[i] >= 'a')
+        if (text[i]=='{' || text[i]=='}' || text[i]=='(' || text[i]==')' || text[i]=='[' || text[i]==']')
         {
-            temp = tolower(text[i]);
-            //printf("%c",temp);
-            modified1[count]=temp;
-            //printf("%c",modified1[count]);
-            //printf("%d\n",count);
-            if(!IsStackFull(&S))
-            {
-                  Push(modified1[count],&S);
-            }
+            modified[count]=text[i];
             count++;
         }
     }
-    //printf("\n%d\n",count);
-    y = strlen(modified1);
-    for(int i=0; i<count; i++)
+
+
+    y = strlen(modified);
+    for(int i=0; i<count;i++)
     {
-        Pop(&c,&S);
-        //printf("%c",c);
-        if(c==modified1[i])
+        printf("%c",modified[i]);
+        if(modified[i]==type1_1 || modified[i]==type2_1 || modified[i]==type3_1)
         {
-            //printf("yes\n\n");
-            yescount++;
+            if(!IsStackFull(&S))
+            {
+                if(modified[i]==type1_1)
+                {
+                    type_1++;
+                    Push(modified[i],&S);
+                    printf("%c type_1++\t%d\n",c,type_1);
+                }
+                else if(modified[i]==type2_1)
+                {
+                    type_2++;
+                    Push(modified[i],&S);
+                    printf("%c type_2++\t%d\n",c,type_2);
+                }
+                else
+                {
+                    type_3++;
+                    Push(modified[i],&S);
+                    printf("%c type_3++\t%d\n",c,type_3);
+                }
+            }
         }
         else
         {
-            //printf("no\n\n");
-            printf("\n\n\t                 -----------------------This is not  palindrome-----------------------\n\n");
-            break;
+            if(!IsStackEmpty(&S))
+            {
+                if(type_1!=0)
+                {
+                    if(modified[i]==type1_2)
+                    {
+                        type_1--;
+                        Pop(&c,&S);
+                        printf("%c type_1--\t%d\n",c,type_1);
+                    }
+                }
+                else if(type_2!=0)
+                {
+                    if(modified[i]==type2_2)
+                    {
+                        type_2--;
+                        Pop(&c,&S);
+                        printf("%c type_2--\t%d\n",c,type_2);
+                    }
+                }
+                else if(type_3!=0)
+                {
+                    if(modified[i]==type3_2)
+                    {
+                        type_3--;
+                        Pop(&c,&S);
+                        printf("%c type_3--\t%d\n",c,type_3);
+                    }
+                }
+            }
         }
-        if(yescount==count){
-            printf("\n\n\t                 -----------------------This is  palindrome-----------------------\n\n");
-        }
-    }printf("\n\n\t                 -----------------------Thank you-----------------------\n\n");
+    }
+
+    printf("%d",type_1);
+    printf("%d",type_2);
+    printf("%d",type_3);
+    if((type_1+type_2+type_3)!=0)
+        printf("\n\n\t\t--------------------That is not correct formula--------------------\n\n");
+    else
+        printf("\n\n\t\t--------------------That is correct formula--------------------\n\n");
+
 }
